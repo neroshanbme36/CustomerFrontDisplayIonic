372a470731d81dbf22c7bc17468de8368a2a8ce9
neroshan <neroshanbme36@outlook.com>
2024-05-29 18:59:17 +0530

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'customer-facing-display-ionic',
+  webDir: 'www'
+};
+
+export default config;
